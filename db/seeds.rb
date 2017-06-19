b8aa31492c9327add6f01204c555ad379ae82b5c
jbci <jbari@ciudadanoineligente.org>
2017-06-19 17:53:46 -0400

    dockerizing app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(email: 'email@example.com', password: '12345678', password_confirmation: '12345678')
+
+Rut.create(rut: '1')
+Rut.create(rut: '2')
+Rut.create(rut: '3')
+Rut.create(rut: '4')
+Rut.create(rut: '5')
+Rut.create(rut: '6')
+Rut.create(rut: '7')
+Rut.create(rut: '8')
+Rut.create(rut: '9')
+Rut.create(rut: '10')
+Rut.create(rut: '11')
+Rut.create(rut: '12')
+Rut.create(rut: '100')
