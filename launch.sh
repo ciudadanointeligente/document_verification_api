977037402699645a349eee64c0b744be5adc3e0d
jbci <jbari@ciudadanoineligente.org>
2017-06-28 18:27:26 -0400

    split export in 2 lines

--- launch.sh
+++ launch.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
-export SECRET_KEY_BASE=$(rails secret)
-export POSTGRESQL_PORT=5433
-export POSTGRESQL_USER=postgres
-export POSTGRESQL_PASSWORD=pgp4ss
-export RAILS_ENV=production
-export RAILS_PORT=3000
+echo "setting env vars for document_verification_api launch"
+
+SECRET_KEY_BASE=$(rails secret)
+POSTGRESQL_PORT=5433
+POSTGRESQL_USER=postgres
+POSTGRESQL_PASSWORD=pgp4ss
+RAILS_ENV=production
+RAILS_PORT=3000
+
+export SECRET_KEY_BASE
+export POSTGRESQL_PORT
+export POSTGRESQL_USER
+export POSTGRESQL_PASSWORD
+export RAILS_ENV
+export RAILS_PORT
+
+echo "starting document_verification_api"
 
 docker-compose up -d
+
+echo "document_verification_api started"
