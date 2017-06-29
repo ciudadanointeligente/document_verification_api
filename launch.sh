877122b1c8a2f0d9f5f08f8323fc206ac0472127
jbci <jbari@ciudadanoineligente.org>
2017-06-29 11:02:24 -0400

    dont generate a secret

--- launch.sh
+++ launch.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 echo "setting env vars for document_verification_api launch"
+echo "running as $(whoami)"
 
-SECRET_KEY_BASE=$(rails secret)
+SECRET_KEY_BASE=16d582c02fc8aee3ca9a6fb3015ba521cba5577b1913140e3870124ca3dbe7a89d5b663c69ffb05118ac60b057a92f0df3aa414327876e8610728c132321ca25
 POSTGRESQL_PORT=5433
 POSTGRESQL_USER=postgres
 POSTGRESQL_PASSWORD=pgp4ss
@@ -18,6 +19,7 @@ export RAILS_PORT
 
 echo "starting document_verification_api"
 
+docker-compose down
 docker-compose up -d
 
 echo "document_verification_api started"
